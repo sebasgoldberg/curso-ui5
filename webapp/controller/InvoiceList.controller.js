2562d69f9d4f90aa6b73dc6a0da155f110c71889
Juan Sebastián Goldberg <sebas.goldberg@gmail.com>
2019-07-16 17:24:04 -0300

    Passo 17: Eliminando no frontend

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -3,8 +3,9 @@ sap.ui.define([
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageToast"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator, MessageToast) {
 	"use strict";
 	return Controller.extend("sgoldberg.sap.ui.demo.walkthrough.controller.InvoiceList", {
 		formatter: formatter, 
@@ -33,6 +34,19 @@ sap.ui.define([
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.navTo("detail", {
 				invoicePath: encodeURIComponent(oItem.getBindingContext("invoice").getPath())
+			});
+		},
+		onInvoiceDelete: function(oEvent) {
+			var oItem = oEvent.getParameter("listItem"),
+				sPath = oItem.getBindingContext('invoice').getPath();
+
+			this.getView().getModel('invoice').remove(sPath,{
+				success: function(){
+					MessageToast.show('Invoice eliminada com sucesso.');
+				}.bind(this),
+				error: function(e){
+					console.error(e);
+				}.bind(this),
 			});
 		}
 	});
